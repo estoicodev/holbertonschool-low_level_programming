e9158cdb3bbed9f908fcc4b03260f1346d393c06
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-04 20:58:22 +0000

    First version of 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - main function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char abc[] = "0123456789";
+int abclen = strlen(abc);
+int i;
+for (i = 0; i < abclen; i++)
+{
+putchar(abc[i]);
+}
+putchar('\n');
+return (0);
+}
