66e93f705ef6d5bd13c6b7bb9dc87a31ce36a983
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-08 17:59:07 +0000

    First version of 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: The number to convert into absolute
+ *
+ * Return: The absolute number.
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (n * -1);
+}
+return (n);
+}
