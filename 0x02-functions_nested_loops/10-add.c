5136348896d6a90b2f2b2db69d194a81c359da3b
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-08 18:52:14 +0000

    First version of 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Sum two numbers
+ * @a: First number
+ * @a: Second number
+ *
+ * Return: The sum of a and b.
+ */
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
