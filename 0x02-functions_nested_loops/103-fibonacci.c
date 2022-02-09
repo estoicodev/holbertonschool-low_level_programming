57854a11d2f0489235d549206f935c486826aa60
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-09 04:47:09 +0000

    First version of 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * fibonacci_even_terms - prints the prints the sum of the even-valued
+ * terms that not exceed 4,000,000
+ * Return: void
+ */
+void fibonacci_even_terms(void)
+{
+int a, b, c, i;
+long int sum;
+a = 1;
+b = 2;
+sum = 0 + b;
+while (c < 4000000)
+{
+c = a + b;
+if (c % 2 == 0)
+{
+sum += c;
+}
+a = b;
+b = c;
+}
+printf("%lu\n", sum);
+}
+
+/**
+ * main - main function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+fibonacci_even_terms();
+return (0);
+}
