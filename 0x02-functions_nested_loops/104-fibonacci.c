cc90e86385d55e738b2af65559ead59079a97ae5
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-09 05:10:41 +0000

    Change types of variables on 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,17 +6,17 @@
  */
 void fibonacci_98(void)
 {
-unsigned long a, b, c;
+unsigned long long a, b, c;
 int i;
 a = 1;
 b = 2;
-printf("%lu, %lu", a, b);
+printf("%llu, %llu", a, b);
 for (i = 0; i < 96; i++)
 {
 c = a + b;
 a = b;
 b = c;
-printf(", %lu", c);
+printf(", %llu", c);
 }
 printf("\n");
 }
