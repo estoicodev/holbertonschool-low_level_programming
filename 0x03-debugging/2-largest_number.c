e8caa34cf060170a361bd35268563a123afed90b
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-10 16:08:52 +0000

    Fix logic in 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,6 +20,18 @@ else if (b > a && b > c)
 {
 largest = b;
 }
+else if (c > a && c > b)
+{
+largest = c;
+}
+else if ((a == b || a == c) && (a > b || a > c))
+{
+largest = a;
+}
+else if ((b == a || b == c) && (b > a || b > c))
+{
+largest = b;
+}
 else
 {
 largest = c;
