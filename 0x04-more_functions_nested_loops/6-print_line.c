8bb57a8d62e6cfe2e3b1ae285431862e1bd99737
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-10 19:33:44 +0000

    First version of 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - prints 10 times the numbers, from 0 to 14
+ * @n: The number of times the character '_' should be printed
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+if (n > 0)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+}
+_putchar('\n');
+}
