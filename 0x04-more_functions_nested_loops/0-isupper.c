08162c40af29af2b11d5caa133af0c11b9e10e1a
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-10 17:22:40 +0000

    Change general return of the function _isupper.c in 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * _isupper -  checks for uppercase character.
- * Return: On success 1.
- * Otherwise, 0
+ * _islower - Checks for uppercase character
+ * @c: The character to evaluate if is upper.
+ *
+ * Return: If is lower returns 1.
+ * Otherwise, 0 is returned.
  */
 int _isupper(int c)
 {
