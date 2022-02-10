6db3bdfa339994eb0718a07c4a475d64aed4da49
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-10 17:02:56 +0000

    First version of 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper -  checks for uppercase character.
+ * Return: On success 1.
+ * Otherwise, 0
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
