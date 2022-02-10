d15fd83fb961a7e251054032dbf28ec0e1228a42
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-10 17:45:11 +0000

    Add protorype in main.h on 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the numbers, from 0 to 9
+ * print_most_numbers - prints the numbers, from 0 to 9 (without 2
+ * and 4)
  *
  * Return: void
  */
