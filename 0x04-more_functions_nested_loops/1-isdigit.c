d0ee167091d4d6bc142b27c3ac9cedf66b7a1934
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-10 17:06:44 +0000

    First version of 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit -  checks for digit character.
+ * Return: On success 1.
+ * Otherwise, 0
+ */
+int _isdigit(int c)
+{
+int i;
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
