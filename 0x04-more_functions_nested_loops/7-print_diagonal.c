d8ac222d4f958ef848cd9b19a6ef8541bc437beb
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-11 15:59:55 +0000

    Add indentation (8 spaces) in 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,25 +8,26 @@
  */
 void print_diagonal(int n)
 {
-if (n > 0)
-{
-int i;
-int j;
-for (i = 0; i < n; i++)
-{
-if (i > 0)
-{
-for (j = 0; j < i; j++)
-{
-_putchar(' ');
-}
-}
-_putchar(92);
-_putchar('\n');
-}
-}
-else
-{
-_putchar('\n');
-}
+	if (n > 0)
+	{
+		int i;
+		int j;
+
+		for (i = 0; i < n; i++)
+		{
+			if (i > 0)
+			{
+				for (j = 0; j < i; j++)
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
