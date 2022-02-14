2302fcf131ca90df21fa1b42456fe8606044948d
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-14 21:45:05 +0000

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,26 +8,17 @@
  */
 void print_rev(char *s)
 {
-	int len;
-
-	len = 0;
 	while (*s != '\0')
 	{
 		_putchar(*s);
-		_putchar('\n');
 		s++;
-		len++;
 	}
+	_putchar('\n');
 
-	while (len >= 0)
+	while (*s - 2)
 	{
 		_putchar(*s);
-		if (len == 0)
-		{
-			break;
-		}
 		s--;
-		len--;
 	}
 	_putchar('\n');
 }
