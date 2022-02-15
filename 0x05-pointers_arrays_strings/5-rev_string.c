a66ca1bcb93e74bb9be22e2c43682b91e11df05f
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-15 17:12:36 +0000

    Delete unnecesary lines in 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * rev_string - Prints a string, in reverse
  * @s: String
@@ -22,27 +21,18 @@ void rev_string(char *s)
 	if (len % 2 != 0)
 	{
 		n = (len - 1) / 2;
-		for (i = 0; i < n; i++)
-		{
-			temp = *s;
-			*s = *p_counter;
-			*(s + len - 1) = temp;
-			s++;
-			p_counter--;
-			len -= 2;
-		}
 	}
 	else
 	{
 		n = len / 2;
-		for (i = 0; i < n; i++)
-		{
-			temp = *s;
-			*s = *p_counter;
-			*(s + len - 1) = temp;
-			s++;
-			p_counter--;
-			len -= 2;
-		}
+	}
+	for (i = 0; i < n; i++)
+	{
+		temp = *s;
+		*s = *p_counter;
+		*(s + len - 1) = temp;
+		s++;
+		p_counter--;
+		len -= 2;
 	}
 }
