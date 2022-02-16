592cc54f7799c6ce8bfa816b9de51f1c8f0cee09
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-16 23:27:37 +0000

    First version of 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: First string
+ * @src: Second string
+ * Return: String after concatenation
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len, i;
+	char *p_count = src;
+
+	while (*dest)
+	{
+		dest++;
+	}
+
+	len = 0;
+	while (*p_count)
+	{
+		len++;
+		p_count++;
+	}
+	for (i = 0; *(src + i); i++)
+	{
+		*dest = *(src + i);
+		dest++;
+		len++;
+	}
+	len--;
+	while (len > 0)
+	{
+		dest--;
+		len--;
+	}
+	return (dest);
+}
