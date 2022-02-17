251cfb2fdbc7748e87d3591e5fa67eca94a918ca
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-17 17:19:09 +0000

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,29 +6,28 @@
  * @src: Second string
  * Return: String after concatenation
  */
-char *_strcat(char *dest, char *src)
+char *_strcat (char *dest, char *src)
 {
-	int len, i;
+        int len;
+        int i;
 
-	len = 0;
-	while (*dest)
-	{
-		dest++;
-		len++;
-	}
-	for (i = 0; *(src + i); i++)
-	{
-		*dest = *(src + i);
-		dest++;
-		len++;
-	}
-	dest -= 2;
-
-	len -= 2;
-	while (len > 0)
-	{
-		dest--;
-		len--;
-	}
-	return (dest);
+        len = 0;
+        while(*dest)
+        {
+                dest++;
+                len++;
+        }
+        for (i = 0; *(src + i); i++)
+        {
+                *dest = *(src + i);
+                dest++;
+                len++;
+        }
+        *dest = '\0';
+        while (len > 0)
+        {
+                dest--;
+                len--;
+        }
+        return (dest);
 }
