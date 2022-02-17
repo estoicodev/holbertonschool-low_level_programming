ae1d256b3e1a33fff0c7351cdfcefda4c47d21d2
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-17 17:25:27 +0000

    Fix betty styles on 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,28 +6,33 @@
  * @src: Second string
  * Return: String after concatenation
  */
-char *_strcat (char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-        int len;
-        int i;
+	int len;
+	int i;
 
-        len = 0;
-        while(*dest)
-        {
-                dest++;
-                len++;
-        }
-        for (i = 0; *(src + i); i++)
-        {
-                *dest = *(src + i);
-                dest++;
-                len++;
-        }
-        *dest = '\0';
-        while (len > 0)
-        {
-                dest--;
-                len--;
-        }
-        return (dest);
+	len = 0;
+
+	while (*dest)
+	{
+		dest++;
+		len++;
+	}
+
+	for (i = 0; *(src + i); i++)
+	{
+		*dest = *(src + i);
+		dest++;
+		len++;
+	}
+
+	*dest = '\0';
+
+	while (len > 0)
+	{
+		dest--;
+		len--;
+	}
+
+	return (dest);
 }
