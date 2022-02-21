c27dea9e307bdf9401c3c97f6b19d8b05cff57e8
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-21 21:45:55 +0000

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,13 +12,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        *(dest + i) = *src;
-        src++;
-    }
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *src;
+		src++;
+	}
 
-    return (dest);
+	return (dest);
 }
