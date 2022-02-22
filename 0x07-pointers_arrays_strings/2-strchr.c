7aad84b6f644aac77559c70aea6f4a2ee78850cc
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-22 04:19:40 +0000

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,17 +9,18 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+        int i;
 
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == c)
-		{
-			return (&s[i]);
-		}
-		i++;
-	}
+        i = 0;
+        while (s[i] != '\0')
+        {
+                if (s[i] == c)
+                {
+                        return (&s[i]);
+                }
 
-	return (NULL);
+                i++;
+        }
+
+        return (NULL);
 }
