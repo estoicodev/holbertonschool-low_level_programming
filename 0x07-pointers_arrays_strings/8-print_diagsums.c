de24d50a40c981642ca6c5101efdb3804104a7b0
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-22 03:41:15 +0000

    Fix betty styles on 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,31 +9,33 @@
  */
 void print_diagsums(int *a, int size)
 {
-        int i, j, len;
-        int count_one, count_two;
-        int sum_one, sum_two;
+	int i, j, len;
+	int count_one, count_two;
+	int sum_one, sum_two;
 
-        len = size * size;
-        count_one = 0;
-        count_two = size - 1;
-        sum_one = 0;
-        sum_two = 0;
+	len = size * size;
+	count_one = 0;
+	count_two = size - 1;
+	sum_one = 0;
+	sum_two = 0;
 
-        for (i = 0; i < len; i++)
-        {
+	for (i = 0; i < len; i++)
+	{
 		if (i == count_one)
 		{
 			sum_one += *(a + i);
 			count_one += size + 1;
 		}
-        }
-        for (j = 0; j < len; j++)
-        {
+	}
+
+	for (j = 0; j < len; j++)
+	{
 		if (j == count_two && j != len - 1)
 		{
 			sum_two += *(a + j);
 			count_two += size - 1;
 		}
-        }
-        printf("%d, %d\n", sum_one, sum_two);
+	}
+
+	printf("%d, %d\n", sum_one, sum_two);
 }
