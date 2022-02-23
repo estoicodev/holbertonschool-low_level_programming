5c05dc212abe0713e496f8dd0c9f69b9c6839060
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-02-23 16:04:01 +0000

    First version of 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_recursion - desc
+ * @s: desc
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(10);
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
