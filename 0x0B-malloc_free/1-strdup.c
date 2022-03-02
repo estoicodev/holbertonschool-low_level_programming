f431771918fbae50a151605cb5f689b22b586634
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-02 03:02:43 +0000

    First v. of 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strdup - desc
+ * @str: desc
+ *
+ * Return: Pointer to the string duplicated
+ */
+char *_strdup(char *str)
+{
+	int size, i;
+	char *s_array;
+
+	if (str == NULL)
+		return (NULL);
+
+	size = 0;
+	while (*(str + size))
+	{
+		size++;
+	}
+
+	s_array = (char *) malloc(size * sizeof(char));
+
+	i = 0;
+	while (i < size)
+	{
+		*(s_array + i) = *(str + i);
+		i++;
+	}
+	*(s_array + i) = '\0';
+
+	return (s_array);
+}
