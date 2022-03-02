77e552f65f92e33411a3452b9b30b3b7a27ad95b
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-02 02:45:48 +0000

    v. 3 of 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	s_array = (char *)malloc(size * sizeof(char));
+	s_array = (char *) malloc(size);
 
 	i = 0;
 	while (i < size)
