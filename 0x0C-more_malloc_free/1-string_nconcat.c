d47a4796a6a6707db75278689f5ab22edd635fdd
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-03 22:59:50 +0000

    change 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,9 +13,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len_s1, len_s2, i, j;
 	char *ptr;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-
 	if (s1 == NULL)
 		s1 = "";
 
