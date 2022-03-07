80eb472dde5449c0f69a428009d17ed66679b3df
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-07 23:50:19 +0000

    Up task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,6 +15,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *n_dog;
 	char *cp_name, *cp_owner;
 
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
 	n_dog = malloc(sizeof(struct dog));
 
 	if (n_dog == NULL)
@@ -23,19 +26,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	n_dog->name = name;
 	n_dog->age = age;
 	n_dog->owner = owner;
-
-	if (n_dog->name == NULL || n_dog->age < 0 || n_dog->owner == NULL)
-		return (NULL);
-
-	cp_name = malloc(sizeof(n_dog->name));
-
-	if (cp_name == NULL)
-		return (NULL);
-
-	cp_owner = malloc(sizeof(n_dog->owner));
-
-	if (cp_owner == NULL)
-		return (NULL);
 
 	return (n_dog);
 }
