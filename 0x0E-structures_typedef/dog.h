60b44e0531f0e70a60b75999c67462e2ec77ce4f
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-07 22:48:41 +0000

    Add betty style into dog.h struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,14 @@
 
 #include <stdio.h>
 
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
