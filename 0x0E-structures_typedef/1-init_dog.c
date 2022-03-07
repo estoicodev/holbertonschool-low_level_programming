a780e056aa248cfd02aab24a378f4353410c62e4
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-07 22:58:18 +0000

    v1 of task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "dog.h"
+
+/**
+ * init_dog - desc
+ * @dog: ...
+ * @name: ...
+ * @age: ...
+ * @owner: ...
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
