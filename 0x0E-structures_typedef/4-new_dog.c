96605a4b08fbd548a2697c07c5019822f7ebd0a7
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-07 23:39:28 +0000

    v1 of task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - desc
+ * @name: ...
+ * @age: ...
+ * @owner: ...
+ *
+ * Return: new type struct dog_t
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *n_dog;
+
+	n_dog = malloc(sizeof(struct dog));
+
+	if (n_dog == NULL)
+		return (NULL);
+
+	n_dog->name = name;
+	n_dog->age = age;
+	n_dog->owner = owner;
+
+	return (n_dog);
+}
