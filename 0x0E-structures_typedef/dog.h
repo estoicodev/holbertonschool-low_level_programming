80d60184247b27b6edab0515d67fe0cd6f37b59b
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-07 23:04:20 +0000

    Up dog.h and task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdio.h>
-
 /**
  * struct dog - Short description
  * @name: First member
@@ -17,8 +15,6 @@ struct dog
 	float age;
 	char *owner;
 };
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
-int _putchar(char c);
 
 #endif
