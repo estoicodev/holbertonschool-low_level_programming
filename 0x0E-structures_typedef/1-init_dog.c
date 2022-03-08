67885f6d2edb6c277a94e5d8f030beb9fedcd7a7
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-08 17:26:43 +0000

    Finish task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,10 +13,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (name == NULL || age <= 0 || owner == NULL)
-		exit(1);
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
