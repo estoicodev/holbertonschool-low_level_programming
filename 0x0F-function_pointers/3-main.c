481c6a92bc37b162dd0cd9b143d7df4f0f39226b
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-09 22:02:41 +0000

    Fix betty styles on task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,17 +7,17 @@
  *
  * Return: 0 (Success)
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-        char *arr[5] = {"+", "-", "*", "/", "%"};
+	char *arr[5] = {"+", "-", "*", "/", "%"};
 	int (*pf)(int a, int b);
-        int res;
+	int res;
 
-        if (argc != 4)
-        {
-                printf("Error\n");
-                exit(98);
-        }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
 	pf = get_op_func(argv[2]);
 
@@ -36,40 +36,6 @@ int main(int argc, char* argv[])
 	}
 
 	printf("%d\n", res);
-        return (0);
-}
-
-/**
-int main(int argc, char* argv[])
-{
-	char *arr[5] = {"+", "-", "*", "/", "%"};
-	int res, j;
-
-	if (argc == 4)
-	{
-		printf("%s\n", argv[0]);
-		printf("%s\n", argv[1]);
-		printf("%s\n", argv[2]);
-		printf("%s\n", argv[3]);
-		printf("----\n");
-
-		if (*argv[2] == *arr[2])
-		{
-			res = atoi(argv[1]) * atoi(argv[3]);
-			printf("%s * %s = %d\n", argv[1], argv[3], res);
-		}
-		else if (*argv[2] == *arr[3])
-		{
-			res = atoi(argv[1]) / atoi(argv[3]);
-			printf("%s / %s = %d\n", argv[1], argv[3], res);
-		}
-		else if (*argv[2] == *arr[4])
-		{
-			res = atoi(argv[1]) % atoi(argv[3]);
-			printf("%s mod %s = %d\n", argv[1], argv[3], res);
-		}
-	}
 
 	return (0);
 }
-*/
