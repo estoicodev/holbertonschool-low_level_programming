e2a88178439062c04f08189ad4d66466016be03e
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-11 05:05:56 +0000

    Up header file to task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,5 +10,20 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(char *a, va_list b);
+void print_int(char *a, va_list b);
+void print_float(char *a, va_list b);
+void print_char_ptr(char *a, va_list b);
+
+/**
+ * struct f_types - Struct
+ * @format: conversion format
+ * @f: function pointer
+ */
+typedef struct f_types
+{
+	char *format;
+	void (*f)(char *a, va_list b);
+} f_t;
 
 #endif
