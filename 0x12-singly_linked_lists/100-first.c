96b53e3930bda63ea3d5fabd4435c9e881aa2e59
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-18 08:44:53 +0000

    v1 of task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void faster(void) __attribute__ ((constructor));
+
+/**
+ * faster - desc
+ *
+ * Return: void
+ */
+void faster(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
