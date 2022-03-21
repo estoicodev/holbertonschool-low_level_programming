c49c209316b2af26e4a978dd2d546870b25db02d
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-21 23:23:11 +0000

    v1 of task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - desc
+ * @h: ...
+ *
+ * Return: void
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
