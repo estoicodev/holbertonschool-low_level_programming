f0d1685ab7f02bd6e8ce284d85aa436cf3ee5609
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-22 13:33:52 +0000

    v1.3 task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,16 +8,16 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current = *head;
+	listint_t *current;
 
 	if (head == NULL)
 		return;
 
 	while (*head)
 	{
-		*head = (*head)->next;
-		free(current);
-		current = *head;
+		current = (*head)->next;
+		free(*head);
+		*head = current;
 	}
 
 	head = NULL;
