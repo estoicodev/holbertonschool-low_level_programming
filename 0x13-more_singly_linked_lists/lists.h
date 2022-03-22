6cbafff3bac3e1e7476dca083a18511f3751d146
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-22 18:18:46 +0000

    Fix betty of header and task 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,8 +5,6 @@
 #include <string.h>
 #include <stdio.h>
 
-int _putchar(char c);
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -17,10 +15,11 @@ int _putchar(char c);
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
