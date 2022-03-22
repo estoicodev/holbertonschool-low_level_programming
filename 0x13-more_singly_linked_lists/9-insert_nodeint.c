f2cb5004a206b5e5119bb4f7239ee8611203d5f6
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-22 16:01:47 +0000

    v2 of task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,59 +1,6 @@
 #include "lists.h"
 
 /**
- * create_nodeint - desc
- * @n: ...
- *
- * Return: void
- */
-listint_t *create_nodeint(int n)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
-
-	return (new);
-}
-
-/**
- * count_listint - desc
- * @head: ...
- *
- * Return: integer
- */
-unsigned int count_listint(listint_t *head)
-{
-	unsigned int count = 0;
-	listint_t *tmp = head;
-
-	while (tmp)
-	{
-		tmp = tmp->next;
-		count++;
-	}
-
-	return (count);
-}
-
-/**
- * go_to_final_node - desc
- * @head: ...
- *
- * Return: void
- */
-listint_t *go_to_final_node(listint_t *head)
-{
-	while (head->next)
-		head = head->next;
-
-	return (head);
-}
-
-/**
  * insert_nodeint_at_index - desc
  * @head: ...
  * @idx: ...
