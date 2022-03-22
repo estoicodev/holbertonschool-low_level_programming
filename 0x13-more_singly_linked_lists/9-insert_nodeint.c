81fa1d565c29d1c26bdb9719031d8afb3ad6e901
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-22 14:48:19 +0000

    v1 of task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,90 @@
+#include "lists.h"
+
+/**
+ * create_nodeint - desc
+ * @n: ...
+ *
+ * Return: void
+ */
+listint_t *create_nodeint(int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+
+	return (new);
+}
+
+/**
+ * count_listint - desc
+ * @head: ...
+ *
+ * Return: integer
+ */
+unsigned int count_listint(listint_t *head)
+{
+	unsigned int count = 0;
+	listint_t *tmp = head;
+
+	while (tmp)
+	{
+		tmp = tmp->next;
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+ * get_nodeint_at_index - desc
+ * @head: ...
+ * @idx: ...
+ * @n: ...
+ *
+ * Return: void
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i, len;
+	listint_t *before, *current;
+	listint_t *tmp = *head;
+	listint_t *new = create_nodeint(n);
+
+	if (new == NULL)
+		return (NULL);
+
+	len = count_listint(*head);
+
+	if (len < idx)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+	{
+		if (idx != 0)
+			if (i == idx - 1)
+				before = tmp;
+
+		if (i == idx)
+		{
+			new->next = tmp;
+			current = tmp;
+			tmp = new;
+
+			if (idx != 0)
+				before->next = tmp;
+			else
+				*head = tmp;
+		}
+
+		if (i > idx)
+			tmp = current;
+
+		tmp = tmp->next;
+	}
+
+	return (new);
+}
