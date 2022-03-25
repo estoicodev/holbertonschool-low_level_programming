9b2178840b7c6a1fbaf2ce79d89ed798f28caf3e
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-25 04:42:31 +0000

    v1 of task 2

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - desc
+ * @n: ...
+ * @index: ...
+ *
+ * Return: void.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = *n ^ (1 << index);
+
+	return (-1);
+}
