91a54e97c595a907dfa2527fc975f95f78e9f483
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-25 17:58:12 +0000

    v1 of task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * flip_bits - desc
+ * @n: ...
+ * @m: ...
+ *
+ * Return: void.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int n_xor_m, active, count = 0;
+
+	n_xor_m = n ^ m;
+
+	while (n_xor_m >= 1)
+	{
+		active = n_xor_m & 1;
+		if (active == 1)
+			count++;
+
+		n_xor_m = n_xor_m >> 1;
+	}
+
+	return (count);
+}
