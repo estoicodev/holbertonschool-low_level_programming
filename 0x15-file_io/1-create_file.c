9c33b881eb9df4e6abc650f0bb13d51f190a527a
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-29 03:51:44 +0000

    v1.1 of task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,6 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, text_len, file_len;
-	char *content;
 
 	if (filename == NULL)
 		return (-1);
@@ -24,11 +23,7 @@ int create_file(const char *filename, char *text_content)
 
 	text_len = _strlen(text_content);
 
-	content = malloc(sizeof(char) * text_len);
-	if (content == NULL)
-		return (-1);
-
-	file_len = write(STDOUT_FILENO, content, text_len);
+	file_len = write(STDOUT_FILENO, text_content, text_len);
 
 	if (file_len != text_len)
 		return (-1);
