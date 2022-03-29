4220f44106e0bc65131e11af00e861f39b60d968
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-29 03:53:36 +0000

    v1.2 of task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 
 	text_len = _strlen(text_content);
 
-	file_len = write(STDOUT_FILENO, text_content, text_len);
+	file_len = write(fd, text_content, text_len);
 
 	if (file_len != text_len)
 		return (-1);
