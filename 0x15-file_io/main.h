c74f2cd7a2053ec2d070f7a6c59de9f7d2f93ae2
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-04-01 05:21:38 +0000

    Add prototype of task 3 in header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,5 +11,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int _strlen(char *s);
 int append_text_to_file(const char *filename, char *text_content);
+int cp_text_file_into_file(const char *filefrom, const char *fileto);
 
 #endif
