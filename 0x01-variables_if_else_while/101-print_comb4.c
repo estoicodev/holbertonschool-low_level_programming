03b66377e419cc13124a986f805afe641739ad7c
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-04-19 22:50:41 +0000

    v1 of task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - main function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int x, y, z, c;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			for (z = '0'; z <= '9'; z++)
+			{
+				if (x == y && x == z && y == z)
+					continue;
+				if (x < y && y < z)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					if (x == '7' && z == '9')
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
