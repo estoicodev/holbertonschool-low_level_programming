6c264da2815464aa4b9bccef033b34252be5e49c
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-05-06 04:14:57 +0000

    v1 of task 7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,59 @@
+#include "lists.h"
+
+/**
+ * create_dnodeint - Create a single node of dlistint_t
+ * @n: Integer
+ *
+ * Return: void
+ */
+dlistint_t *create_dnodeint(int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+
+	return (new);
+}
+/**
+ * insert_dnodeint_at_index - Inserts a new node at a given position
+ * @h: Pointer to the head of a dlistint_t list
+ * @idx: Index to add the node
+ * @n: Integer (n value)
+ *
+ * Return: void
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *tmp = *h;
+	unsigned int i;
+
+	new = create_dnodeint(n);
+	if (new == NULL)
+		return (NULL);
+
+	if (idx == 0)
+	{
+		new->next = tmp;
+		tmp->prev = new;
+		*h = new;
+		return (new);
+	}
+
+	for (i = 0; i < (idx - 1); i++)
+	{
+		if (tmp == NULL)
+			return (NULL);
+
+		tmp = tmp->next;
+	}
+
+	new->next = tmp->next;
+	tmp->next = new;
+	new->prev = tmp;
+
+	return (new);
+}
