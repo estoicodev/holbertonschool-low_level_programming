6c3568987c27ebcd1c065f021fcd8abb5418019d
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-05-06 04:56:46 +0000

    v2.1 of task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,6 +35,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 
+	if (!*h)
+		return (NULL);
+
 	if (idx == 0)
 	{
 		new->next = tmp;
