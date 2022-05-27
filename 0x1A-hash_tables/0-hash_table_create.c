4354c612d94867a627522acdd0d483b344653011
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-05-27 05:43:27 +0000

    v1 of task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_t - creates a hash table
+ * @size: size of the table
+ *
+ * Return: pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * ht->size);
+	if (!(ht->array))
+	{
+		free(ht);
+		return (NULL);
+	}
+	for (i = 0; i < ht->size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
+}
