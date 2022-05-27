d6bc025f2fee92f2d800eb2f03ec56fa38f9af70
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-05-27 07:29:22 +0000

    v1.2 of task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -73,35 +73,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((const unsigned char *)key, ht->size);
 	current_item = ht->array[index];
 	if (current_item == NULL)
-	{
 		ht->array[index] = item;
-	}
 	else
 	{
-		if (strcmp(current_item->key, key) == 0)
+		while (current_item != NULL)
 		{
-			free_hash_node(item);
-			free(ht->array[index]->value);
-			ht->array[index]->value = value_cpy;
-			return (1);
-		}
-		else
-		{
-			while (current_item != NULL)
+			if (strcmp(current_item->key, key) == 0)
 			{
-				if (strcmp(current_item->key, key) == 0)
-				{
-					free_hash_node(item);
-					free(current_item->value);
-					current_item->value = value_cpy;
-					return (1);
-				}
-				current_item = current_item->next;
+				free_hash_node(item);
+				free(current_item->value);
+				current_item->value = value_cpy;
+				return (1);
 			}
-			item->next = ht->array[index];
-			ht->array[index] = item;
-			return (1);
+			current_item = current_item->next;
 		}
+		item->next = ht->array[index];
+		ht->array[index] = item;
 	}
 
 	return (1);
