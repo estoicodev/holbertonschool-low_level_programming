0ad3a9c594fb9e985c62489649c40f7d65654861
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-08-29 15:06:08 -0500

    v1 of task 0 (and push header file)

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Entry point
+ * @array: ...
+ * @size: ...
+ * @value: ...
+ * Return: Always EXIT_SUCCESS
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i = 0;
+
+    if (array == NULL)
+        return (-1);
+
+    for (i = 0; i < size; i++)
+    {
+        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+        if (array[i] == value)
+            return (i);
+    }
+    return (-1);
+}
