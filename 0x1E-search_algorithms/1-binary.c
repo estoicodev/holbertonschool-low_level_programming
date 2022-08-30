8cacd15d5e80bf022de1e4aae3fb35051090c886
Mauricio2802 <vmcarrasco2810@gmail.com>
2022-08-30 16:52:29 -0500

    v2 in task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -19,9 +19,14 @@ int binary_search(int *array, size_t size, int value)
 
 	while (low <= high)
 	{
-		printf("Searching in array:");
+		printf("Searching in array: ");
 		for (i = low; i <= high; i++)
-			printf(", %d", array[i]);
+		{
+			printf("%d", array[i]);
+			if (i < high)
+				printf(", ");
+
+		}
 		printf("\n");
 
 		if (array[mid] == value)
